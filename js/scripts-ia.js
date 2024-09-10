84a3113783b7a9dd05c13cec3f134d807a296ff0
cheimienst hernandez <cheimienst18@gmail.com>
2024-09-09 22:22:52 -0300

    update

--- js/scripts-ia.js
+++ js/scripts-ia.js
@@ -1,6 +1,10 @@
+console.log('funcina esta monda')
 function scrollToSection(sectionId) {
-    const section = document.getElementById(sectionId);
+    const section = document.querySelector(sectionId);
     if (section) {
         section.scrollIntoView({ behavior: 'smooth' });
+    } else {
+        console.error(`No se encontró la sección con el ID: ${sectionId}`);
     }
 }
+
